10ae132d0487c013d219676a81257c1e1f680628
Evgeny P <livin9000@gmail.com>
2022-06-30 09:11:05 +0500

    fonts

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="w-1/3 flex flex-col items-center">
-        <img :src="require(`@/assets/images/${img}`)" alt="">
-        <div>{{ name }}</div>
-        <div>{{ price }}</div>
+    <div class="w-full flex flex-col items-center">
+        <img class="mb-4 rounded-md cursor-pointer hover:scale-102 duration-300" :src="require(`@/assets/images/${img}`)" alt="">
+        <div class="uppercase mb-1">{{ name }}</div>
+        <div class="font-light text-gray-500">{{ price }}</div>
     </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        'gray': {
-          50: '#fafafa',
-          100: '#f5f5f5',
-          200: '#e5e5e5',
-          300: '#d4d4d4',
-          400: '#a3a3a3',
-          500: '#737373',
-          600: '#525252',
-          700: '#404040',
-          800: '#262626',
-          900: '#171717',
+    content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                'serif': ["Noto Serif"],
+                'sans': ["Montserrat"],
+                'display': ["Noto Serif Display"]
+            },
+            scale: {
+                '102': '1.02'
+            },
+            colors: {
+                'mainRed': '#EC0121',
+                'gray': {
+                    50: '#fafafa',
+                    100: '#f5f5f5',
+                    150: '#f1f1f1',
+                    200: '#e5e5e5',
+                    300: '#d4d4d4',
+                    400: '#a3a3a3',
+                    500: '#737373',
+                    600: '#525252',
+                    700: '#404040',
+                    800: '#262626',
+                    900: '#171717',
+                },
+            },
+            spacing: {
+                '400': '60rem',
+            }
         },
-      },
-      spacing: {
-        '400': '60rem',
-      }
     },
-  },
-  plugins: [],
+    plugins: [],
 }
